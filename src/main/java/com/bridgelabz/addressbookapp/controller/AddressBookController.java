fbcd8b83ae0285307676d1acf73d764131d300de
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-09-20 00:14:07 +0530

    [Bhupendra]ADD:Addressbook data pojo class

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -1,27 +1,53 @@
 package com.bridgelabz.addressbookapp.controller;
 
 import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
+import com.bridgelabz.addressbookapp.dto.ResponseDTO;
+import com.bridgelabz.addressbookapp.model.AddressBookData;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @RestController
 @RequestMapping("/book")
 public class AddressBookController {
-
-@RequestMapping(value = {"" ,"/","/get"})
-    public ResponseEntity<String> getAddressBookData(@PathVariable("id") int id){
-    return new ResponseEntity<String>("Get Call Success for id :"+id, HttpStatus.OK);
+    /**
+     * Get -localhost:8081/book/get
+     * @return
+     * {
+     *     "message": "Get Call Successfull",
+     *     "data": {
+     *         "id": 1,
+     *         "fullName": "Bhupendra",
+     *         "address": "Amla",
+     *         "city": "Amla",
+     *         "state": "MP",
+     *         "emailId": "wadekar.bhupendra6@gmail.com",
+     *         "zip": "460551",
+     *         "phoneNumber": "9893902047"
+     *     }
+     * }
+     */
+    @RequestMapping(value = {"" ,"/","/get"})
+    public ResponseEntity<ResponseDTO> getAddressBookData(){
+    AddressBookData addressBookData=null;
+    addressBookData=new AddressBookData(1,new AddressBookDTO(1,"Bhupendra","Amla","Amla","MP","wadekar.bhupendra6@gmail.com","460551","9893902047"));
+    ResponseDTO respDTO=new ResponseDTO("Get Call Successfull",addressBookData);
+    return new ResponseEntity<ResponseDTO>(respDTO, HttpStatus.OK);
 }
 
     /**
      * Get-localhost:8081/book/get/1
      * @param id
-     * @return
+     * @return -Get call Success for id :1
      */
     @GetMapping("/get/{Id}")
-    public ResponseEntity<String> getAddressBookDataById(@PathVariable("Id") int id){
-    return new ResponseEntity<String>("Get call Success for id :"+id,HttpStatus.OK);
+    public ResponseEntity<ResponseDTO> getAddressBookDataById(@PathVariable("Id") int id){
+        AddressBookData addressBookData=null;
+        addressBookData=new AddressBookData(1,new AddressBookDTO(1,"Bhupendra","Amla","Amla","MP","wadekar.bhupendra6@gmail.com","460551","9893902047"));
+        ResponseDTO respDTO=new ResponseDTO("Get Call By Id Successfull",addressBookData);
+      return new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
 }
 
     /**
@@ -40,10 +66,16 @@ public class AddressBookController {
      * @param addressBookDTO
      * @returnCreate AddressBook Data for :AddressBookDTO{id=1, fullName='Bhupendra', address='Amla', city='Amla', state='MP', emailId='bsjsldknmk', zip='7897', phoneNumber='98709009'}
      */
+    //{
+    //    "message": "Created AddressBook Data Succesfully ",
+    //    "data": null
+    //}
     @PostMapping("/create")
-    public ResponseEntity<String> addAddresBookData(
+    public ResponseEntity<ResponseDTO> addAddresBookData(
         @RequestBody AddressBookDTO addressBookDTO){
-    return  new ResponseEntity<String>("Create AddressBook Data for :"+addressBookDTO,HttpStatus.OK);
+        AddressBookData addressBookData=null;
+        ResponseDTO respDTO=new ResponseDTO("Created AddressBook Data Succesfully ", addressBookData);
+    return  new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
 }
 
     /**
@@ -53,9 +85,11 @@ public class AddressBookController {
      */
 
     @PutMapping("/update")
-    public ResponseEntity<String> updateAddresBookData(
+    public ResponseEntity<ResponseDTO> updateAddresBookData(
             @RequestBody AddressBookDTO addressBookDTO){
-        return  new ResponseEntity<String>("Update AddressBook Data for :"+addressBookDTO,HttpStatus.OK);
+        AddressBookData addressBookData=null;
+        ResponseDTO respDTO=new ResponseDTO("Update AddressBook Data Succesfully ", addressBookData);
+        return  new ResponseEntity<ResponseDTO>(respDTO,HttpStatus.OK);
     }
 
     /**
--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.addressbookapp.model;
 
+import com.bridgelabz.addressbookapp.dto.AddressBookDTO;
+
 public class AddressBookData {
     private int id;
     private String fullName;
@@ -7,6 +9,84 @@ public class AddressBookData {
     private String city;
     private String state;
     private String emailId;
-    private int  zip;
+    private String zip;
     private String  phoneNumber;
+
+    public AddressBookData(){}
+
+    public AddressBookData(int id, AddressBookDTO addressBookDTO){
+        this.id=id;
+        this.fullName=addressBookDTO.fullName;
+        this.address=addressBookDTO.address;
+        this.city=addressBookDTO.city;
+        this.state=addressBookDTO.state;
+        this.emailId=addressBookDTO.emailId;
+        this.zip=addressBookDTO.zip;
+        this.phoneNumber=addressBookDTO.phoneNumber;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
